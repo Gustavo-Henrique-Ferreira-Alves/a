f2c800f00fcd7075586257b8dd97ea7ac200cac7
Gustavo-Henrique-Ferreira-Alves <gustavohferreiraalves@gmail.com>
2021-08-09 21:50:15 -0300

    ddl, dml e dql finalizado

--- exercicios/1.4-exercicio-optus/scripts/1.4-exercicio-optus_DML.sql
+++ exercicios/1.4-exercicio-optus/scripts/1.4-exercicio-optus_DML.sql
@@ -1,4 +1,4 @@
-USE STUDIO;
+USE ARTISTA_OPTUS;
 GO
 
 INSERT INTO EMPRESA(nomeEmpresa, endereco)
@@ -10,17 +10,17 @@ VALUES (1,'MC Rick'),(2,'L7nnon');
 GO
 
 INSERT INTO ALBUM(idArtista, nomeAlbum, datalançamento, localizacao, quantidadeMinutos, ativoVisualização)
-VALUES (2,'HHR','HHR','16-07-2021','RJ','60','sim'),(1,'RickWeek', 'RickWeek', '18-07-2021', 'MG', '70', 'nao');
+VALUES (2,'HHR','16-07-2021','RJ','60', 1),(1, 'RickWeek', '18-07-2021', 'MG', '70', 1);
 GO
 
-INSERT INTO CD(idAlbum, nomeCD, numeroCd)
-VALUES (1,'Faca valer','10'),(2,'Posso te fazer chora','20');
+INSERT INTO ESTILO(nomeEstilo)
+VALUES ('Trap'),('Acustico'),('Rap'),('Funk');
 GO
 
-INSERT INTO ESTILO(idCd, nomeEstilo, nomeEstilo1, nomeEstilo2)
-VALUES (1,'Trap','Acustico','Rap'),(2,'Funk','Trap','');
+INSERT INTO USUARIO(idEmpresa, nomeUsuario, nomeEmail, senha, tipoPermissao)
+VALUES (1,'Gustavo','gustavo@gmail','1234', 1),(2,'kaik','kaik@gmail','123', 0);
 GO
 
-INSERT INTO USUARIO(idEmpresa, nomeUsuario, nomeEmail,senha, tipoPermissao)
-VALUES (1,'Gustavo','gustavo@gmail','1234','comum'),(2,'kaik','kaik@gmail','123','administrador');
+INSERT INTO ESTILO_ALBUM(idAlbum, idEstilo)
+VALUES (1,3), (2,4);
 GO
